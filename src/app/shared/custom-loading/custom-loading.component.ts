c3327abf7b6d1318737e9d043a37c6918feae1ef
miguelzabalaf <zabalafmiguel@hotmail.com>
2021-03-27 23:57:24 -0500

    Add custom loading to show characters details

--- src/app/shared/custom-loading/custom-loading.component.ts
+++ src/app/shared/custom-loading/custom-loading.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-custom-loading',
   templateUrl: './custom-loading.component.html',
   styleUrls: ['./custom-loading.component.scss']
 })
-export class CustomLoadingComponent implements OnInit {
+export class CustomLoadingComponent implements OnInit, OnDestroy {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnDestroy(): void {
+    document.documentElement.style.overflow = "auto";
   }
 
+  ngOnInit(): void {
+    document.documentElement.style.overflow = "hidden";
+  }
 }
